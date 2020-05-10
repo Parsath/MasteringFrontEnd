e14f37847587e2341bd731b9a6202c8f82589e70
Parsath <bilel.taktak@esprit.tn>
2020-05-10 20:32:47 +0100

    Update

---
+++ Setting up Angular/tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, of} from 'rxjs';
+import { MessageService } from './message.service'
+import {HEROES} from "./mock-heroes";
+import {Hero} from "./hero";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  constructor(private messageService: MessageService) { }
+
+
+}
---
+++ Setting up Angular/tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Bakugo' },
+  { id: 12, name: 'All Might' },
+  { id: 13, name: 'Eraserhead' },
+  { id: 14, name: 'Lemilion' },
+  { id: 15, name: 'Endeavor' },
+  { id: 16, name: 'Shoto' },
+  { id: 17, name: 'Luffy' },
+  { id: 18, name: 'Zoro' },
+  { id: 19, name: 'Mugen' },
+  { id: 20, name: 'Zenitsu' }
+]
