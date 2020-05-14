9397a3c5c12ee3bab186180078a76e7bc25ae1d4
Parsath <bilel.taktak@esprit.tn>
2020-05-14 21:59:27 +0100

    Update

--- Setting up Angular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Setting up Angular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { MessageService } from "../message.service";
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService,
@@ -23,11 +22,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
     // this.heroes = this.heroService.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
 }
